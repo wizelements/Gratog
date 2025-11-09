f94767b91fd09d4fa4c9fee2da3f2a3a0d03a6ba
Solomon Watkins <silverwatkins@gmail.com>
2025-11-09 01:06:42 +0000

    auto-commit for a1d08098-a156-4cd7-9180-7578b561476e

--- app/order/page-old.js
+++
@@ -1,8 +0,0 @@
-// Server component wrapper - forces dynamic rendering
-export const dynamic = 'force-dynamic';
-
-import OrderPageClient from './OrderPage.client';
-
-export default function OrderPage() {
-  return <OrderPageClient />;
-}
