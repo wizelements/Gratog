cf50492304d6337adb89243f7a1fabaf296fcdd2
Nomolos Sniktaw <silverwatkins@gmail.com>
2026-04-19 09:41:26 -0400

    fix: add queue_joined and queue_join_failed to AnalyticsEvent type
    Additional events used in ReviewAndPay.tsx but missing from type definition.

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -9,6 +9,8 @@ export type AnalyticsEvent =
   | 'checkout_stage_change'
   | 'checkout_proceed_to_payment'
   | 'checkout_validation_failed'
+  | 'queue_joined'
+  | 'queue_join_failed'
   | 'fulfillment_type_selected'
   | 'contact_completed'
   | 'payment_initiated'
